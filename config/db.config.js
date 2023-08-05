a4faec92f0115a7fc722eb29246c6b9da3dd6446
WereWolf1408 <blackshark9999@mail.ru>
2023-08-05 12:09:37 +0300

    add postgress config

---
+++ config/db.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  database: "Tosha",
+  login: "postgres",
+  password: "admin",
+  options: {
+    host: "localhost",
+    dialect: "postgres",
+    port: 5432,
+    logging: false,
+  },
+};
--- routers/common.routes.js
+++ routers/common.routes.js
@@ -1,12 +1,21 @@
 const urlsHolder = require('../config/urls');
+const User = require('../models/users');
 
 const commonRoutes = (app) => {
   app.get("/", (req, res) => {
     res.redirect(urlsHolder.common.main);
   });
 
-  app.get("/example", (req, res) => {
-    res.redirect("/media");
+  app.get("/example", async (req, res) => {
+    try {
+      const users = await User.findAll();
+      console.log(users);
+      res.send('');
+    } catch (error) {
+      console.log(error);
+      res.send(error.message);
+    }
+    
   });
 
   app.get("/about", (req, res) => {
